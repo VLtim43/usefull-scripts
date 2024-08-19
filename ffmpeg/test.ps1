8e00de3b456e8ee470466dec0f58928c0eed2d55
VLtim43 <fernandoreal.valentim@gmail.com>
2024-08-18 21:42:43 -0300

    folders

--- ffmpeg/test.ps1
+++ ffmpeg/test.ps1
@@ -1,14 +1,38 @@
 
+
 #read .env
-Get-Content .env | ForEach-Object {
-    $name, $value = $_.split('=')
-    if ([string]::IsNullOrWhiteSpace($name) -or $name.Contains('#')) {
-        continue
-    }
-    Set-Content env:\$name $value
-}
+# Get-Content .env | ForEach-Object {
+#     $name, $value = $_.split('=')
+#     if ([string]::IsNullOrWhiteSpace($name) -or $name.Contains('#')) {
+#         continue
+#     }
+#     Set-Content env:\$name $value
+# }
 
 #set input folder
-$folderPath = $env:PATH
+# $folderPath = $env:PATH
 
-Write-Output $folderPath
+$folderPath = Read-Host 'Enter the path'
+
+$fileTypes = @("*.png", "*.jpg", "*.jpeg", "*.webp", "*.gif", "*.mp4", "*.avi", "*.mkv")
+$fileCounts = @{}
+
+foreach ($fileType in $fileTypes) {
+    $fileCounts[$fileType] = 0
+}
+
+$totalFiles = (Get-ChildItem -Path $folderPath -File -Recurse | Measure-Object).Count
+Write-Output "Total files: $totalFiles"
+
+$totalFolders = (Get-ChildItem -Path $folderPath -Directory -Recurse | Measure-Object).Count
+Write-Output "Total folders: $totalFolders"
+
+foreach ($fileType in $fileTypes) {
+    $count = (Get-ChildItem -Path $folderPath -Filter $fileType -File -Recurse | Measure-Object).Count
+    $fileCounts[$fileType] = $count
+
+    if ($fileCounts[$fileType] -ne 0) {
+        $extension = $fileType.TrimStart("*.")
+        Write-Output "$extension $($fileCounts[$fileType])"
+    }
+}
